277249337b87c3f16f0472b3eb55ef08870e325c
milic12 <milic00@fesb.hr>
2020-06-24 11:11:22 +0200

    Updates

---
+++ precache-manifest.8ba49246eab4012779bcb66eefd672ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07ab37a6e8a0bbf7e0fac8ac6ffafa",
+    "url": "/covid19-app-reactJS/index.html"
+  },
+  {
+    "revision": "edc5121fa196ddc7d422",
+    "url": "/covid19-app-reactJS/static/css/main.fbe55d77.chunk.css"
+  },
+  {
+    "revision": "f45c280baf77d98ce40f",
+    "url": "/covid19-app-reactJS/static/js/2.b6d9b4e1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-app-reactJS/static/js/2.b6d9b4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc5121fa196ddc7d422",
+    "url": "/covid19-app-reactJS/static/js/main.33391472.chunk.js"
+  },
+  {
+    "revision": "bde55fc3db30a0036dc3",
+    "url": "/covid19-app-reactJS/static/js/runtime-main.9005b15e.js"
+  },
+  {
+    "revision": "dc3deff7df9f42143044aeed00beca7e",
+    "url": "/covid19-app-reactJS/static/media/coronaworld4.dc3deff7.png"
+  },
+  {
+    "revision": "362b2de50074bc04ab9ae3734d8774c0",
+    "url": "/covid19-app-reactJS/static/media/whitecovid19.362b2de5.jpg"
+  }
+]);
