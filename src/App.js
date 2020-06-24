6418fc6259b8889c0539cf80dfab51cc5bcda8e3
milic12 <milic00@fesb.hr>
2020-06-24 10:37:41 +0200

    deploy react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Cards from './components/Cards/Cards';
+//import Cards from './components/Chart/Chart';
+//import Cards from './components/CountryPicker/CountryPicker';
+
+import{Cards, Chart, CountryPicker} from'./components'; //Jednostavnije nacin importanja za razliku od gornjeg nacina, povezan sa index.js u componentsu
+import styles from './App.module.css';
+import { fetchData } from './api';
+import coronaImage from './images/coronaworld4.png';
+class App extends React.Component {
+    state= {
+        data: {},
+        country: '',    
+    }
+//componentDidMount ova se metoda koristi za dohvacanje podataka iz exstenog api-a async koristimo zbog await-a
+    async componentDidMount(){
+        const data = await fetchData();
+
+        this.setState({data});
+    }
+
+    handleCountryChange = async (country) => {
+        const data = await fetchData(country);
+
+        this.setState({data, country:country });
+    }
+
+
+    render(){
+        const { data, country } = this.state;
+
+        return(
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID19"/>
+                <Cards data= {data} />
+                <Chart data= {data} country={country}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+            </div>
+              );
+            
+            }
+
 }
 
 export default App;
